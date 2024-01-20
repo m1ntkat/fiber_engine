28cdf5c83600343ed83f7305209dfb49172b74b6
mintkat <85989407+m1ntkat@users.noreply.github.com>
2024-01-20 13:51:46 -0900

    Update tab_container.gd

--- editor/tab_container.gd
+++ editor/tab_container.gd
@@ -5,6 +5,8 @@ extends TabContainer
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	tab_bar.tab_close_display_policy = TabBar.CLOSE_BUTTON_SHOW_ACTIVE_ONLY
+	tab_bar.tab_close_pressed.connect(remove_current_tab)
 
 
-
+func remove_current_tab(t: int):
+	get_current_tab_control().queue_free() 
