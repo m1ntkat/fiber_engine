09e4cc45433a8a1c988d156e428f86caec0eef73
mintkat <85989407+m1ntkat@users.noreply.github.com>
2024-01-20 12:23:33 -0900

    Started work on the editor, Spkg format

---
+++ resources/project_config.gd
@@ -0,0 +1,6 @@
+extends Resource
+class_name ProjectConfig
+
+var name : String
+
+var type : SpkgPackage.PackageType
---
+++ resources/spkg/spkg_package.gd
@@ -0,0 +1,11 @@
+extends Resource
+class_name SpkgPackage
+
+enum PackageType {
+	Game,
+	Addon
+}
+
+var name : String
+
+var type : PackageType
